4729b74d17f9f1d45423ef1cf77939d30354b5e7
Santiago Torricella <torrisantiago@gmail.com>
2022-10-04 11:32:54 -0300

    UI + Logica completada

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const btnPiedra = document.querySelector("#piedra");
+const btnPapel = document.querySelector("#papel");
+const btnTijera = document.querySelector("#tijera");
+const resultadosDiv = document.querySelector(".resultadosDiv");
+const resultado = document.createElement("div");
+const puntuacion = document.createElement("div");
+const puntuacionMaquina = document.createElement("div");
+const ganador = document.createElement("div");
+
+resultado.classList.add("resultadoJuego");
+puntuacion.classList.add("puntuacionHumano");
+
+btnPiedra.addEventListener("click", () => console.log(playRound("Piedra")));
+btnPapel.addEventListener("click", () => console.log(playRound("Papel")));
+btnTijera.addEventListener("click", () => console.log(playRound("Tijera")));
+
+resultadosDiv.appendChild(resultado);
+resultadosDiv.appendChild(puntuacion);
+resultadosDiv.appendChild(puntuacionMaquina);
+resultadosDiv.appendChild(ganador);
+
+/*
+==== FUNCION QUE TOMA LA MANO DE LA COMPUTADORA ====
+*/
+
 function getComputerChoice() {
   const opciones = ["Piedra", "Papel", "Tijera"];
   let rand = Math.random();
@@ -9,6 +37,10 @@ function getComputerChoice() {
   console.log(opcion);
   return opcion;
 }
+
+/*
+==== FUNCION QUE TOMA LA MANO DEL JUGADOR ====
+*/
 
 function playerSelection() {
   let manoJugador = prompt("Elija su mano");
@@ -22,40 +54,57 @@ function playerSelection() {
   manoFinal = primeraLetra.toUpperCase() + manoContinuacion.toLowerCase();
   return manoFinal;
 }
+/*
+==== FUNCION QUE JUEGA UNA RONDA ====
+*/
 
 function playRound(playerSelect, computerSelect) {
-  playerSelect = playerSelection();
   computerSelect = getComputerChoice();
 
   if (playerSelect === computerSelect) {
-    return "A tie game";
+    resultado.textContent = "A tie game";
   }
 
   if (playerSelect === "Piedra" && computerSelect === "Papel") {
     computerScore++;
-    return "You Lose, Papel beats Rock";
+    resultado.textContent = "You Lose, Papel beats Rock";
   } else if (playerSelect === "Piedra" && computerSelect === "Tijera") {
     humanScore++;
-    return "You Win, Rocks beats Scisors";
+    resultado.textContent = "You Win, Rocks beats Scisors";
   }
 
   if (playerSelect === "Papel" && computerSelect === "Tijera") {
     computerScore++;
-    return "You Lose, Scisors beats Papel";
+    resultado.textContent = "You Lose, Scisors beats Papel";
   } else if (playerSelect === "Papel" && computerSelect === "Piedra") {
     humanScore++;
-    return "You Win, Papel beats Rock";
+    resultado.textContent = "You Win, Papel beats Rock";
   }
 
   if (playerSelect === "Tijera" && computerSelect === "Piedra") {
     computerScore++;
-    return "You Lose, Rock beats Scisors";
+    resultado.textContent = "You Lose, Rock beats Scisors";
   } else if (playerSelect === "Tijera" && computerSelect === "Papel") {
     humanScore++;
-    return "You Win, Scisors beats Papel";
+    resultado.textContent = "You Win, Scisors beats Papel";
+  }
+  puntuacion.textContent = humanScore;
+  puntuacionMaquina.textContent = computerScore;
+
+  if (humanScore === 5) {
+    ganador.textContent = "El humano gana";
+  }
+
+  if (computerScore === 5) {
+    ganador.textContent = "La maquina es la ganadora";
   }
 }
 
+/*
+==== FUNCION QUE REALIZA EL JUEGO ====
+*/
+
+/*
 function game() {
   for (i = 0; i < 5; i++) {
     console.log(playRound());
@@ -68,8 +117,4 @@ function game() {
     console.log("You win the game mate!");
   }
 }
-
-let humanScore = 0;
-let computerScore = 0;
-
-game();
+*/
